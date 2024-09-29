92447e3699753e1c29889366828c02357f2d2dcb
AbulFozolJumman <abulfozoljumman@gmail.com>
2024-09-29 23:57:04 +0600

    Banner image added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import imgd from "@/assets/de964b06-e1de-42a8-8442-d571e44feb6b.png";
+import Image from "next/image";
+
 const HomePage = () => {
   return (
     <div>
       <h1 className="text-4xl text-center mt-10">Welcome To Driving Center</h1>
+      <Image
+        height={1000}
+        width={500}
+        src={imgd}
+        alt="Drive"
+        className="mx-auto mt-10"
+      />
     </div>
   );
 };
